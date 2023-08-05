525c5f8c8783ab7072fe5e171f7dac1d37267380
guyostfeld <guyostfeld1@gmail.com>
2023-08-05 23:17:35 +0300

    Update basicWifiConnection.ino
    pretty doc

--- examples/basicWifiConnection/basicWifiConnection.ino
+++ examples/basicWifiConnection/basicWifiConnection.ino
@@ -1,27 +1,41 @@
+// Include the core Lab-Thing library
 #include "lab-thing.h"
-/*
- * 1. add this program code to your code.
- * 2. your device should now create a wifi Access Point when started.
- * 3. connect to this AP using the AP_PASSWORD defined below.
- * 4. go to the config page (using the url below) and configure the local wifi credentials and a device ID
- * 5. restart the esp (the credentials and ID are saved in the EEPROM)
- * 6. you can now control this esp GPIO's with the url's below, from any device connected to the same wifi network.
 
-   led -- http://lab_thing_ID.local/4/cmd/write?val=0&duration=1000 // try this to turn on built in led for 1000 millis...
-   wifi settings -- http://lab_thing_ID.local/wifi // configure wifi settings
-   OTA -- http://lab_thing_ID.local/firamware_update // press ctrl+alt+s then upload the bin file to web page (saved in sketch directory)
+/**
+ * This script allows your device to create a wifi Access Point (AP) on startup.
+ * 
+ * To use, follow the steps:
+ * 1. Merge this script with your existing code.
+ * 2. On startup, your esp device will create a WIFI AP (labthing_default). 
+ * 3. Connect to this AP using the AP_PASSWORD (defined below).
+ * 4. Go to the config page (URL mentioned below) to provide the local WiFi credentials and device ID.
+ * 5. Restart the ESP8266. (This allows the EEPROM to save the credentials and ID).
+ * 6. Your ESP GPIO's can now be controlled by the URL's below from any device on the wifi network. Enjoy!
+ * 
+ * URL Endpoints:
+ * - Control LED: http://lab_thing_<ID>.local/4/cmd/write?val=0&duration=1000 (Sets the built-in LED to HIGH for 1000 millis)
+ * - WiFi Settings: http://lab_thing_<ID>.local/wifi (Configure Wi-Fi settings)
+ * - Firmware Updates (OTA): http://lab_thing_<ID>.local/firmware_update (Press CTRL+ALT+S, then upload the .bin file located in the sketch directory)
+ * note that the DNS can sometimes cause problems, if the above URL's does not work it is recommended using the device ip instead (printed to the serial monitor).
+ */
 
-*/
-
-#define AP_PASSWORD "12345678"
+// Constant definitions for WiFi Access Point and OTA Update Authentication
+#define AP_PASSWORD        "12345678"
 #define OTA_AUTH_USER_NAME "admin"
-#define OTA_AUTH_PASSWORD "admin"
+#define OTA_AUTH_PASSWORD  "admin" // CHANGE THIS!
 
-void setup() {
+void setup() 
+{
+  // Begin serial communication at a baudrate of 115200
   Serial.begin(115200);
+
+  // Begin the Lab-Thing instance with the necessary credentials
   LabThing.begin(AP_PASSWORD, OTA_AUTH_USER_NAME, OTA_AUTH_PASSWORD);
 }
 
-void loop() {  // dont use delay here
+void loop() 
+{
+  // Continously run the Lab-Thing library's functionalities
+  // IMPORTANT: Do not use delay() here as it might interfere with the timing of LabThing functions
   LabThing.run();
 }
