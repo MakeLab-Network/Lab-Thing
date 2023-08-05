a4ab7f6f635c6e0c0fb893ffff9466b6246f5db9
guyostfeld <guyostfeld1@gmail.com>
2023-08-05 21:51:26 +0300

    Add doc

--- examples/basicWifiConnection/basicWifiConnection.ino
+++ examples/basicWifiConnection/basicWifiConnection.ino
@@ -1,8 +1,12 @@
 #include <LabThing.h>
 
 /*
-   these urls should be accecible trough wifi network, although some routers Oppose...
-   available URLs: (replace ID with your device's id)
+ * 1. add this program code to your code.
+ * 2. your device should now create a wifi Access Point when started.
+ * 3. connect to this AP using the AP_PASSWORD defined below.
+ * 4. go to the config page (using the url below) and configure the local wifi credentials and a device ID
+ * 5. restart the esp (the credentials and ID are saved in the EEPROM)
+ * 6. you can now control this esp GPIO's with the url's below, from any device connected to the same wifi network.
 
    led -- http://lab_thing_ID.local/4/cmd/write?val=0&duration=1000 // try this to turn on built in led for 1000 millis...
    wifi settings -- http://lab_thing_ID.local/wifi // configure wifi settings
